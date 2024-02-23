7cd296a6413ea750876cb822edb161389f420e0f
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-02-23 12:41:12 +0200

    Add files via upload

---
+++ 04.ExerciseInheritance/01.Person/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Person
+{
+    public class Person
+    {
+		private int age;
+
+		private string name;
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		public virtual int Age
+		{
+			get { return age; }
+			set
+			{
+				if (age<0)
+				{
+					throw new Exception();
+				} 
+				age = value; 
+			}
+		}
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.Append(String.Format("Name: {0}, Age: {1}",this.Name,this.Age));
+
+            return stringBuilder.ToString();
+        }
+
+    }
+}
---
+++ 04.ExerciseInheritance/01.Person/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using _01.Person;
+
+public class StarUp
+{
+    public static void Main(string[] args)
+    {
+        string name = Console.ReadLine();
+        int age = int.Parse(Console.ReadLine());
+
+        if (age<=15)
+        {
+            Child person = new Child(name, age);
+            Console.WriteLine(person.ToString());
+        }
+        else
+        {
+            Person person = new Person(name, age);
+            Console.WriteLine(person.ToString());
+        }
+    }
+}
