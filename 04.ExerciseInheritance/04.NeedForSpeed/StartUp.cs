6676935573e415dc3ff769dbb87455e4a659a866
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-02-23 12:42:48 +0200

    Add files via upload

---
+++ 04.ExerciseInheritance/04.NeedForSpeed/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using NeedForSpeed;
+
+namespace NeedForSpeed;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        CrossMotorcycle car = new CrossMotorcycle(100, 100);
+        car.Drive(9);
+        Console.WriteLine(car.Fuel);
+    }
+}
