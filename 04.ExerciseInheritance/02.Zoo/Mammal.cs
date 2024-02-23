7efc0fe74a4b2e7cef2280ed8135fa8f25726e62
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-02-23 12:41:58 +0200

    Add files via upload

---
+++ 04.ExerciseInheritance/02.Zoo/Mammal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zoo;
+
+namespace _02.Zoo
+{
+    public class Mammal : Animal
+    {
+        public Mammal(string name)
+            : base(name)
+        {
+
+        }
+    }
+}
