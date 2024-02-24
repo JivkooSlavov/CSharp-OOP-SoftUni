f18b9fc6d0961115383c52c3bbd6611b958bf832
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-02-24 06:38:09 +0200

    Add files via upload

---
+++ 05.Encapsulation/01.Persons/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Person> persons = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string firstName = input[0];
+                string lastName = input[1];
+                int age = int.Parse(input[2]);
+                Person person = new Person(firstName, lastName, age);
+                persons.Add(person);
+            }
+
+            foreach (var person in persons.OrderBy(x=>x.FirstName).ThenBy(y=>y.Age))
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
