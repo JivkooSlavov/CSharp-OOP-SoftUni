1a231d734ac5576b2dcfc79ae67f6f7ae3e6a6c1
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-02-24 06:38:52 +0200

    Add files via upload

---
+++ 05.Encapsulation/02.Salary/StartUp.cs
@@ -0,0 +1,30 @@
+﻿namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Person> persons = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string firstName = input[0];
+                string lastName = input[1];
+                int age = int.Parse(input[2]);
+                decimal salary = decimal.Parse(input[3]);
+                Person person = new Person(firstName, lastName, age, salary);
+                persons.Add(person);
+            }
+
+            decimal percentage = decimal.Parse(Console.ReadLine());
+            foreach (var person in persons)
+            {
+                person.IncreaseSalary(percentage);
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
