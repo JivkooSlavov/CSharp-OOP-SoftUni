fb31d289aaf3e9ab3e21a8b3133f05efcbdd40b4
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-02-25 22:13:46 +0200

    Add files via upload

---
+++ 6.ExerciseEncapsulation/04.PizzaCalories/Program.cs
@@ -0,0 +1,42 @@
+﻿using _04.PizzaCalories;
+using System.Xml.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] inputPizza = Console.ReadLine().Split();
+        string name = inputPizza[1];
+
+        string[] inputDough = Console.ReadLine().Split();
+        string type = inputDough[0];
+        string flourType = inputDough[1];
+        string backingTechnique = inputDough[2];
+        int weight = int.Parse(inputDough[3]);
+        try
+        {
+
+
+            Dough dough = new Dough(flourType, backingTechnique, weight);
+            Pizza pizza = new Pizza(name, dough);
+
+            string input = "";
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] toppingInfo = input.Split();
+                string toppingType = toppingInfo[1];
+                int toppingWeight = int.Parse(toppingInfo[2]);
+
+                Topping topping = new Topping(toppingType, toppingWeight);
+                pizza.AddTopping(topping);
+            }
+
+            Console.WriteLine($"{pizza.Name} - {pizza.Calories:f2} Calories.");
+        }
+        catch (Exception ex)
+        {
+
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
