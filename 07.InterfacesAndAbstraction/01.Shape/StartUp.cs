d04ae74ffde27cf8af56e2de30265bc4abcc4df4
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-02-28 18:39:04 +0200

    Add files via upload

---
+++ 07.InterfacesAndAbstraction/01.Shape/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            IDrawable circle = new Circle(5);
+            IDrawable rectangle = new Rectangle(5, 7);
+
+            circle.Draw();
+            rectangle.Draw();
+        }
+    }
+}
