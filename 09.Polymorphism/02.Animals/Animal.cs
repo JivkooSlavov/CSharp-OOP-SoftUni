7cad8e7acdbb23e850436d03e550ead4b58e2ba8
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-03-01 23:13:38 +0200

    Add files via upload

---
+++ 09.Polymorphism/02.Animals/Animal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Animals
+{
+    public class Animal
+    {
+        public string name;
+        public string favouriteFood;
+
+        public Animal(string name, string favouriteFood)
+        {
+            Name = name;
+            FavouriteFood = favouriteFood;
+        }
+
+        public string Name { get; set; }
+        public string FavouriteFood {  get; set; }  
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {name} and my fovourite food is {favouriteFood}";
+        }
+    }
+}
