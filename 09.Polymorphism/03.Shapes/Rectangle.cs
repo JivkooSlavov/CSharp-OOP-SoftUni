92fb44f56efa9b9d0a5edf9ac7eeb4b391339561
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-03-01 23:13:53 +0200

    Add files via upload

---
+++ 09.Polymorphism/03.Shapes/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        public Rectangle(double height, double width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public double Height { get; private set; }
+        public double Width { get; private set; }
+        public override double CalculateArea()
+        {
+          return Height* Width;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return Height+Width+Height+Width;
+        }
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+        }
+    }
+}
