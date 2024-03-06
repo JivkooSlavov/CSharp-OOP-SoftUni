59411479033ff541e1eb5447a0654d239f0163b5
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-03-06 10:25:51 +0200

    Add files via upload

---
+++ 11.ExceptionHandling/02.EnterNumbers/Program.cs
@@ -0,0 +1,44 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> numbers = new List<int>();
+
+        while (numbers.Count!=10)
+        {
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number<=1 || number>=100)
+                {
+                    int lastNumber = numbers.LastOrDefault()==0 ? 1 : numbers.LastOrDefault();
+
+                    throw new ArgumentOutOfRangeException("", $"Your number is not in range {lastNumber} - 100!");
+                }
+                else if (numbers.Count==0)
+                {
+                    numbers.Add(number);
+                }
+                else if (numbers.LastOrDefault()<number)
+                {
+                    numbers.Add(number);
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("", $"Your number is not in range {numbers.LastOrDefault()} - 100!");
+                }
+
+            }
+            catch (FormatException ex)
+            {
+
+                Console.WriteLine("Invalid Number!");
+            }
+            catch(ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        Console.WriteLine(string.Join(", ", numbers));
+    }
+}
