ebc50d0d099500364993dabb61e4b5e82ebae70b
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-03-06 10:25:23 +0200

    Add files via upload

---
+++ 11.ExceptionHandling/01.SquareRoot/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+		try
+		{
+			int number = int.Parse(Console.ReadLine());
+			if (number<0)
+			{
+				throw new ArgumentOutOfRangeException();
+			}
+            Console.WriteLine(Math.Sqrt(number));
+        }
+		catch (Exception ex)
+		{
+            Console.WriteLine("Invalid number.");
+        }
+		finally
+		{
+            Console.WriteLine("Goodbye.");
+        }
+    }
+}
