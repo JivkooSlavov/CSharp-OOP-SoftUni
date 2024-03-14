817081c93928fdd7f7e25041ccc163f5d226a968
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-03-14 21:15:40 +0200

    Add files via upload

---
+++ 16.ExerciseReflectionAndAttributes/ValidationAttributes/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using ValidationAttributes.Models;
+using ValidationAttributes.Utils;
+using Validator = ValidationAttributes.Utils.Validator;
+
+namespace ValidationAttributes
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var person = new Person
+             (
+                 null,
+                 -1
+             );
+
+            bool isValidEntity = Validator.IsValid(person);
+
+            Console.WriteLine(isValidEntity);
+        }
+    }
+}
