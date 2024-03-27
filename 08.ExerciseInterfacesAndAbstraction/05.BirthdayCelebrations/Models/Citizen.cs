348a82b292252ff1a7010a905e195625439742c4
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-03-27 22:22:43 +0200

    Add files via upload

---
+++ 08.ExerciseInterfacesAndAbstraction/05.BirthdayCelebrations/Models/Citizen.cs
@@ -0,0 +1,27 @@
+﻿using _05.BirthdayCelebrations.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.BirthdayCelebrations.Models
+{
+    public class Citizen : INameable, IIdentifiable, IBirthable
+    {
+        public Citizen(string name, int age, string id, string birthdate)
+        {
+            Name = name;
+            Id = id;
+            Birthdate = birthdate;
+            Age = age;
+        }
+
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public string Id { get; private set; }
+
+        public string Birthdate { get; private set; }
+
+    }
+}
---
+++ 08.ExerciseInterfacesAndAbstraction/05.BirthdayCelebrations/Models/Robot.cs
@@ -0,0 +1,22 @@
+﻿using _05.BirthdayCelebrations.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.BirthdayCelebrations.Models
+{
+    public class Robot : INameable, IIdentifiable
+    {
+        public Robot(string name, string id)
+        {
+            Name = name;
+            Id = id;
+        }
+
+        public string Name { get; private set; }
+
+        public string Id { get; private set; }
+    }
+}
